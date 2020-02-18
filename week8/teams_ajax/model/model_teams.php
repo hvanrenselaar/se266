7da9730a5abd5ca49723433faf73961622548d2c
Erik van Renselaar <erik@neit.edu>
2020-02-18 13:22:34 -0500

    Week 8 Ajax Insert team

--- week8/teams_ajax/model/model_teams.php
+++ week8/teams_ajax/model/model_teams.php
@@ -20,19 +20,22 @@
     function addTeam ($t, $d) {
         global $db;
         
+        $id = -1;
         $stmt = $db->prepare("INSERT INTO teams SET teamName = :team, division = :division");
-
+        
         $binds = array(
             ":team" => $t,
             ":division" => $d
         );
             
         if ($stmt->execute($binds) && $stmt->rowCount() > 0) {
-            $results = 'Data Added';
+            $id = $db->lastInsertId();
         }
         
-        return ($results);
+        return ($id);
     }
+    
+  
     
     function updateTeam ($id, $team, $division) {
         global $db;
